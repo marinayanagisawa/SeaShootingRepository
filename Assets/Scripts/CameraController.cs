de5200aab849ee4967684de2607d788089c4b76c
marinayanagisawa <marinayanagisawacb@gmail.com>
2016-10-04 16:24:10 +0900

    CameraControllerクラスの追加
    カメラがプレイヤーを追従し、画面端はインスペクターから指定する形に変更
    Scene１に仮背景などを追加

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	private GameObject player;
+	private GameObject camera;
+
+	//カメラの移動制限範囲をステージごとに指定
+	public float maxX = 20.0f;
+	public float minX = -20.0f;
+	public float maxY = 3.0f;
+	public float minY = -3.0f;
+
+
+	void Start () {
+		player = GameObject.Find ("Player");
+		camera = GameObject.Find ("Main Camera");
+	}
+	
+
+	void Update () {
+		//カメラのY軸は-10で固定（Vector3）してプレイヤーを追従
+		camera.transform.position = new Vector3 (Mathf.Clamp (player.transform.position.x, minX , maxX),Mathf.Clamp ( player.transform.position.y, minY, maxY),-10);
+
+	}
+
+
+
+}
