e802946505398fe89d9cfd41b8e06ca0a8ab7608
marinayanagisawa <marinayanagisawacb@gmail.com>
2016-10-06 18:49:41 +0900

    背景スクロールを追加（テスト）
    テスト用のテクスチャ、マテリアルを追加
    背景スクロールのスクリプトを追加
    テストシーンの調整

---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroll : MonoBehaviour {
+
+	public float speed = 0.1f;
+
+	void Start () {
+
+	}
+
+	void Update () {
+	
+
+		float x = Mathf.Repeat (Time.time * speed, 1);
+
+		Vector2 offset = new Vector2 (x , 0);
+
+		GetComponent<Renderer> ().sharedMaterial.SetTextureOffset ("_MainTex", offset);
+	}
+}
