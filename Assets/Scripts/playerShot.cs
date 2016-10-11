3841635fbc42bec5ddfd20b5c1a1660081cb8394
marinayanagisawa <marinayanagisawacb@gmail.com>
2016-10-11 18:18:20 +0900

    プレイヤーの弾、スクロールの微調整
    スクロールスピード、弾のLifeTimeなど

--- Assets/Scripts/playerShot.cs
+++ Assets/Scripts/playerShot.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class playerShot : MonoBehaviour {
 
 	private float speed = 12.0f;
-	private float lifeTime = 2.0f;
+	private float lifeTime = 1.5f;
 	private Rigidbody2D rb;
 
 	void Start () {
